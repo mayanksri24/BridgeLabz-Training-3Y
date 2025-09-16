2ccc3171cdfe1ab398128d5a74a32d4c1533d2a4
Mayank Shree <mayank.shree24@gmail.com>
2025-09-16 20:34:57 +0530

    Create 12. Sum of n natural numbers (while loop).java

---
+++ Java Control Flow/Level01/12. Sum of n natural numbers (while loop).java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class NaturalSumWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n > 0) {
+            int sumFormula = n * (n + 1) / 2;
+            int sumLoop = 0, i = 1;
+            while (i <= n) {
+                sumLoop += i;
+                i++;
+            }
+            System.out.println("Formula Sum = " + sumFormula + ", Loop Sum = " + sumLoop);
+        } else System.out.println("Not a natural number");
+        sc.close();
+    }
+}
