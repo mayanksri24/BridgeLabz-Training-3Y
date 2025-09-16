6dff72a4ad5cd5d867885c60e09d10d6d5087d35
Mayank Shree <mayank.shree24@gmail.com>
2025-09-17 01:08:13 +0530

    Create 6.Vowels & Consonants with character type.java

---
+++ Java Strings/Level02/6.Vowels & Consonants with character type.java
@@ -0,0 +1,41 @@
+
+import java.util.Scanner;
+
+public class VowelConsonantType {
+
+    public static String checkCharType(char ch) {
+        if (ch >= 'A' && ch <= 'Z') ch = (char)(ch + 32);
+        if (ch >= 'a' && ch <= 'z') {
+            if ("aeiou".indexOf(ch) != -1) return "Vowel";
+            else return "Consonant";
+        }
+        return "Not a Letter";
+    }
+
+    public static String[][] classifyCharacters(String text) {
+        String[][] result = new String[text.length()][2];
+        for (int i = 0; i < text.length(); i++) {
+            result[i][0] = String.valueOf(text.charAt(i));
+            result[i][1] = checkCharType(text.charAt(i));
+        }
+        return result;
+    }
+
+    public static void display(String[][] arr) {
+        System.out.printf("%-10s %-15s\n", "Char", "Type");
+        System.out.println("----------------------");
+        for (String[] row : arr) {
+            System.out.printf("%-10s %-15s\n", row[0], row[1]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a text: ");
+        String text = sc.nextLine();
+
+        String[][] result = classifyCharacters(text);
+        display(result);
+        sc.close();
+    }
+}
