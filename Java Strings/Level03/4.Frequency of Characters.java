4aa700aab8362b7fbd48d40ee745e424172d3863
Mayank Shree <mayank.shree24@gmail.com>
2025-09-17 01:15:10 +0530

    Create 4.Frequency of Characters.java

---
+++ Java Strings/Level03/4.Frequency of Characters.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+
+public class FrequencyDemo {
+
+    public static String[][] findFrequency(String text) {
+        int[] freq = new int[256];
+
+        for (int i = 0; i < text.length(); i++) {
+            freq[text.charAt(i)]++;
+        }
+
+        String[][] result = new String[text.length()][2];
+        int index = 0;
+
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if (freq[c] != 0) {
+                result[index][0] = String.valueOf(c);
+                result[index][1] = String.valueOf(freq[c]);
+                freq[c] = 0;
+                index++;
+            }
+        }
+
+        // Trim
+        String[][] trimmed = new String[index][2];
+        for (int i = 0; i < index; i++) trimmed[i] = result[i];
+
+        return trimmed;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String text = sc.nextLine();
+
+        String[][] freq = findFrequency(text);
+        System.out.println("Character Frequencies:");
+        for (String[] row : freq) {
+            System.out.println(row[0] + " : " + row[1]);
+        }
+
+        sc.close();
+    }
+}
