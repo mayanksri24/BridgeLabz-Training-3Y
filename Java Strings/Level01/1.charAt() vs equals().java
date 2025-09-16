ba39c3596e39341afe69dd9b6788a29b428b26a3
Mayank Shree <mayank.shree24@gmail.com>
2025-09-17 00:37:30 +0530

    Update and rename 1 to 1.charAt() vs equals().java

---
+++ Java Strings/Level01/1.charAt() vs equals().java
@@ -0,0 +1,28 @@
+1.Compare Two Strings Using charAt() vs equals()
+import java.util.Scanner;
+
+public class StringCompareCharAt {
+    public static boolean compareStrings(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first string: ");
+        String str1 = sc.next();
+        System.out.print("Enter second string: ");
+        String str2 = sc.next();
+
+        boolean userDefinedResult = compareStrings(str1, str2);
+        boolean builtInResult = str1.equals(str2);
+
+        System.out.println("User-defined comparison result: " + userDefinedResult);
+        System.out.println("Built-in equals() result: " + builtInResult);
+
+        sc.close();
+    }
+}
