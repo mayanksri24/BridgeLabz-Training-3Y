20dc040a5fbb4ab6e505f95a8ff7e100727d99c3
Mayank Shree <mayank.shree24@gmail.com>
2025-09-17 00:43:00 +0530

    Create 8. ArrayIndexOutOfBoundsException.java

---
+++ Java Strings/Level01/8. ArrayIndexOutOfBoundsException.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class ArrayIndexOutOfBoundsDemo {
+    public static void generateException(String[] arr) {
+        System.out.println(arr[arr.length]); // out of range
+    }
+
+    public static void handleException(String[] arr) {
+        try {
+            System.out.println(arr[arr.length]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Caught ArrayIndexOutOfBoundsException!");
+        } catch (RuntimeException e) {
+            System.out.println("Caught RuntimeException!");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] names = new String[3];
+        System.out.println("Enter 3 names:");
+        for (int i = 0; i < 3; i++) {
+            names[i] = sc.next();
+        }
+        // generateException(names);
+        handleException(names);
+        sc.close();
+    }
+}
