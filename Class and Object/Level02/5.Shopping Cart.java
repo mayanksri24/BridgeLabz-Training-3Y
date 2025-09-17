fdd14cd35cf5210892917c423e3c4c8537d4838b
Mayank Shree <mayank.shree24@gmail.com>
2025-09-17 08:48:18 +0530

    Create 5.Shopping Cart.java

---
+++ Class and Object/Level02/5.Shopping Cart.java
@@ -0,0 +1,43 @@
+package com.bridgelabz.oops.leveltwo;
+
+class Cart {
+    String item;
+    int qty;
+    double rate;
+
+    Cart(String i, double r, int q) {
+        item = i;
+        rate = r;
+        qty = q;
+    }
+
+    void addItem(int q) {
+        qty += q;
+        System.out.println(q + " " + item + " added.");
+    }
+
+    void removeItem(int q) {
+        if (q <= qty) {
+            qty -= q;
+            System.out.println(q + " " + item + " removed.");
+        } else {
+            System.out.println("Not enough items to remove.");
+        }
+    }
+
+    double total() {
+        return qty * rate;
+    }
+
+    void info() {
+        System.out.println(item + " | Price: " + rate + " | Qty: " + qty + " | Total: " + total());
+    }
+
+    public static void main(String[] args) {
+        Cart c = new Cart("Laptop", 60000, 1);
+        c.info();
+        c.addItem(2);
+        c.removeItem(1);
+        c.info();
+    }
+}
