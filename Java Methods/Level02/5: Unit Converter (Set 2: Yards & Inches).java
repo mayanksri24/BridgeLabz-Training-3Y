29326a17491837e0d2ab68f8b4a18a9bd92cfed8
Mayank Shree <mayank.shree24@gmail.com>
2025-09-17 07:47:44 +0530

    Create 5: Unit Converter (Set 2: Yards & Inches).java

---
+++ Java Methods/Level02/5: Unit Converter (Set 2: Yards & Inches).java
@@ -0,0 +1,26 @@
+public class UnitConverter2 {
+    public static double convertYardsToFeet(double yards) {
+        return yards * 3;
+    }
+
+    public static double convertFeetToYards(double feet) {
+        return feet * 0.333333;
+    }
+
+    public static double convertMetersToInches(double meters) {
+        return meters * 39.3701;
+    }
+
+    public static double convertInchesToMeters(double inches) {
+        return inches * 0.0254;
+    }
+
+    public static double convertInchesToCm(double inches) {
+        return inches * 2.54;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("2 yards = " + convertYardsToFeet(2) + " feet");
+        System.out.println("24 inches = " + convertInchesToCm(24) + " cm");
+    }
+}
