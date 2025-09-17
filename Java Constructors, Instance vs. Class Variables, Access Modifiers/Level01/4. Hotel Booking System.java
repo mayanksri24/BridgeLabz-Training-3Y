97ccd3188f37f9c49d647549c1ed2f94d5acfae9
Mayank Shree <mayank.shree24@gmail.com>
2025-09-17 08:58:43 +0530

    Create 4. Hotel Booking System.java

---
+++ Java Constructors, Instance vs. Class Variables, Access Modifiers/Level01/4. Hotel Booking System.java
@@ -0,0 +1,41 @@
+
+public class HotelBooking {
+    String guestName;
+    String roomType;
+    int nights;
+
+    // Default constructor
+    public HotelBooking() {
+        this.guestName = "Unknown";
+        this.roomType = "Standard";
+        this.nights = 1;
+    }
+
+    // Parameterized constructor
+    public HotelBooking(String guestName, String roomType, int nights) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+        this.nights = nights;
+    }
+
+    // Copy constructor
+    public HotelBooking(HotelBooking other) {
+        this.guestName = other.guestName;
+        this.roomType = other.roomType;
+        this.nights = other.nights;
+    }
+
+    public void display() {
+        System.out.println("Guest: " + guestName + ", Room: " + roomType + ", Nights: " + nights);
+    }
+
+    public static void main(String[] args) {
+        HotelBooking b1 = new HotelBooking();
+        HotelBooking b2 = new HotelBooking("Rahul", "Deluxe", 3);
+        HotelBooking b3 = new HotelBooking(b2); // copy
+
+        b1.display();
+        b2.display();
+        b3.display();
+    }
+}
